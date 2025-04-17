d1e345647fb5c4e790abcfe5479a77562706df99
xe slv <xe.slv@yandex.ru>
2025-04-17 22:11:34 +0300

    brand new b&&***

--- frontend/src/features/LeftCol.jsx
+++ frontend/src/features/LeftCol.jsx
@@ -1,14 +1,38 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Participants from '../components/receipt/participants.jsx';
 import Summary from '../components/receipt/summary.jsx';
+import Sberspasibo from '../components/receipt/sberspasibo.jsx';
 // import { sendReceiptToBackend } from '../services/api'; 
 import styles from './LeftCol.module.css';
 
-const LeftCol = ({totalAmount, participants, onAddParticipant, serviceFee, discount, tips, amount}) => {
+const LeftCol = ({
+    totalAmount, 
+    participants, 
+    onAddParticipant, 
+    serviceFee, 
+    discount, 
+    tips, 
+    amount, 
+    available, 
+    selectedBonus, 
+    setSelectedBonus
+}) => {
     return (
         <div className={styles.leftColContainer}>
             <Participants participants={participants} onAddParticipant={onAddParticipant} />
-            <Summary totalAmount={totalAmount} serviceFee={serviceFee} discount={discount} tips={tips} amount={amount}/>
+            <Sberspasibo 
+                available={available} 
+                min={0} 
+                onChange={setSelectedBonus} 
+            />
+            <Summary 
+                totalAmount={totalAmount} 
+                serviceFee={serviceFee} 
+                discount={discount} 
+                tips={tips} 
+                amount={amount} 
+                available={selectedBonus} 
+            />
         </div>
     );
 };
