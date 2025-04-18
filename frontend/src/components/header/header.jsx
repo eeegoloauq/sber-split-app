136c31b5eff6ec68a3f5706c5fe6ce48f7bd9159
Lalande <Marc.Lalande07@gmail.com>
2025-04-18 21:17:32 +0300

    ee

--- frontend/src/components/header/header.jsx
+++ frontend/src/components/header/header.jsx
@@ -1,21 +1,25 @@
-import React from 'react';
-import styles from './header.module.css'
-import logo from '../../assets/logo.png'
+import React from "react";
+import styles from "./header.module.css";
+import logo from "../../assets/logo.png";
 
 function Header() {
-    return (
-        <header className={styles.header}>
-            <div className={styles.headerContainer}>
-                <a href = "/" className={styles.headerBrandLink}>
-                    <img src={logo} alt="СберСплит" className={styles.headerLogo} />
-                </a>
+  return (
+    <header className={styles.header}>
+      <div className={styles.headerContainer}>
+        <a href="/" className={styles.headerBrandLink}>
+          <img src={logo} alt="СберСплит" className={styles.headerLogo} />
+        </a>
 
-                <nav className={styles.navLinks}>
-                    <a href="/history" className={styles.navLink}>История</a>
-                    <a href="/login" className={styles.navLink}>Войти</a>
-                </nav>
-            </div>
-        </header>
-    )
+        <nav className={styles.navLinks}>
+          <a href="/history" className={styles.navLink}>
+            История
+          </a>
+          <a href="/login" className={styles.navLink}>
+            Войти
+          </a>
+        </nav>
+      </div>
+    </header>
+  );
 }
 export default Header;
